fe05c7ff993f2d99f2c2ed2570c80a325e5a0732
Ascender <ascenderr11@gmail.com>
2026-06-18 11:37:56 +0300

    fix: register auth page locale dictionaries in index.ts

--- src/pages/auth/index.ts
+++ src/pages/auth/index.ts
@@ -1,2 +1,10 @@
+import { registerDictionary } from '@/shared/i18n'
+
+import en from './locale/en.json'
+import ru from './locale/ru.json'
+
+registerDictionary('ru', ru)
+registerDictionary('en', en)
+
 export { LoginPage } from './ui/LoginPage'
 export { RegisterPage } from './ui/RegisterPage'
